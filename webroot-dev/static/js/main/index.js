c6eaad18e0b9001c5de745e77dd0bd9c015065ff
#云淡然 <cloudcome@163.com>
2016-07-24 14:38:48 +0800

    init

--- webroot-dev/static/js/main/index.js
+++ webroot-dev/static/js/main/index.js
@@ -7,6 +7,22 @@
 
 'use strict';
 
+var Slider = require('blear.ui.slider');
+var layout = require('blear.core.layout');
+
+var buildSlider = function () {
+    var bannerRatio = 1366/377;
+    var bannerWidth = layout.width(document);
+    var bannerHeight = bannerWidth/bannerRatio;
+    new Slider({
+        el: '#banner',
+        width: bannerWidth,
+        height: bannerHeight
+    });
+};
 
 
-
+// ========================================================
+// ========================================================
+// ========================================================
+buildSlider();
